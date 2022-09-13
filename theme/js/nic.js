13bf1af3a252b98d9d6e06265626f4609f78ead2
Nic <github@nicolasdeville.com>
2022-09-13 10:48:53 +0200

    1048 test js and boilerplate update

--- theme/js/nic.js
+++ theme/js/nic.js
@@ -3,6 +3,8 @@ const svgCopy =
 const svgCheck =
     '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true"><path fill-rule="evenodd" fill="rgb(63, 185, 80)" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>';
 
+const checkBorder = "1px solid #3FB950"
+
 const addCopyButtons = (clipboard) => {
     // 1. Look for pre > code elements in the DOM
     document.querySelectorAll("pre > code").forEach((codeBlock) => {
@@ -16,7 +18,7 @@ const addCopyButtons = (clipboard) => {
         () => {
             button.blur();
             button.innerHTML = svgCheck;
-            button.style.border = "1px solid #3FB950"; // 220913 test
+            button.style.border = checkBorder; // 220913 test
             setTimeout(() => (button.innerHTML = svgCopy), 3000);
         },
         (error) => (button.innerHTML = "Error")
